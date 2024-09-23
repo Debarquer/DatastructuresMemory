acb0db58c282dc35dfc0a8efcd6eacb44d97fa35
Simon Ektjärn <debarquer@gmail.com>
2024-09-23 13:11:47 +0200

    test: Add ProgramTests

--- DatastructuresMemoryTests/ProgramTests.cs
+++ DatastructuresMemoryTests/ProgramTests.cs
@@ -5,6 +5,52 @@ namespace DatastructuresMemoryTests
 {
 	public class ProgramTests
 	{
+		[Fact]
+		public void TestValidStrings()
+		{
+			string[] validTestStrings =
+			{
+				"(())",
+				"{}",
+				"[({})]",
+				"List<int> list = new List<int>() { 1, 2, 3, 4 };"
+			};
 
+			bool isValid = true;
+			foreach (string s in validTestStrings)
+			{
+				if (!DatastructuresMemory.DatastructuresMemory.ValidateParanthesisString(s))
+				{
+					isValid = false;
+					break;
+				}
+			}
+
+			Assert.True(isValid);
+		}
+
+		[Fact]
+		public void TestInvalidStrings()
+		{
+			string[] invalidTestStrings =
+			{
+				"(()])",
+				"[)",
+				"{[()}]",
+				"List<int> list = new List<int>() { 1, 2, 3, 4 );"
+			};
+
+			bool isValid = false;
+			foreach (string s in invalidTestStrings)
+			{
+				if (DatastructuresMemory.DatastructuresMemory.ValidateParanthesisString(s))
+				{
+					isValid = true;
+					break;
+				}
+			}
+
+			Assert.False(isValid);
+		}
 	}
 }
